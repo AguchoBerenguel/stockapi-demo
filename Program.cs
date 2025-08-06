3c02db52429e55d345400e9cd67a1662084e2012
Agustín <agustinberenguel1999@gmail.com>
2025-08-06 17:58:00 -0300

    Program corregido

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-using StockApi.Data;
+using StockApi.Data; 
 using StockApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Cargar cadena de conexión desde variable de entorno o appsettings
+
 var connectionString = Environment.GetEnvironmentVariable("MYSQLCONN") 
     ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configurar DBContext con MySQL
+
 builder.Services.AddDbContext<StockDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-// Configurar CORS para permitir llamadas desde tu frontend
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -30,6 +31,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Middlewares
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
@@ -43,13 +45,13 @@ app.UseSwaggerUI();
 
 app.MapControllers();
 
-// Aplicar migraciones al iniciar
+
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<StockDbContext>();
     context.Database.Migrate(); 
 }
 
-// 🔁 Usar puerto dinámico en Railway
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Run($"http://0.0.0.0:{port}");
